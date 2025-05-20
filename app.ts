38ef4358a7ca61752d045f8f13b0dcdc80c57321
Aardvark <highcrypto098@gmail.com>
2025-05-20 05:58:17 -0700

    feat: added config import

--- app.ts
+++ app.ts
@@ -4,3 +4,13 @@ const app = express();
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
+
+//----------------------------------------------------------********************************_____________________________________
+
+import { UrlGenerator } from "./utils/nlpProcessor";
+import * as dotenv from "dotenv";
+import { Worker } from "worker_threads";
+import * as path from "path";
+import { ResultURLData } from "./types/type";
+
+dotenv.config();
