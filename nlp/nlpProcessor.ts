6508d40bc4ace4d176060bfe073e3af5eedf8411
Aardvark <highcrypto098@gmail.com>
2025-05-20 05:29:42 -0700

    feat: added class structure

--- nlp/nlpProcessor.ts
+++ nlp/nlpProcessor.ts
@@ -7,3 +7,14 @@ import {
   ResultParams,
 } from "../types/type";
 import { Pinecone } from "@pinecone-database/pinecone";
+
+//define URL generator for nlp processor
+export class UrlGenerator {
+  private openai: OpenAI;
+  private pineconeassist: Pinecone;
+
+  constructor(apiKey: string, pineconeApiKey: string) {
+    this.openai = new OpenAI({ apiKey: apiKey });
+    this.pineconeassist = new Pinecone({ apiKey: pineconeApiKey });
+  }
+}
