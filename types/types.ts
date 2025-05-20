af0a79b314593ad7d4f8934fdeac4f940c2efe7e
Aardvark <highcrypto098@gmail.com>
2025-05-20 05:12:52 -0700

    feat: added dateparams interface for openai analysis

--- types/types.ts
+++ types/types.ts
@@ -3,3 +3,16 @@ export interface ApiResponse<T> {
   data: T;
   error?: string;
 }
+
+//Date param interface
+export interface DateParams {
+  wdi_years: {
+    start: number;
+    end: number;
+  };
+  imf_years: string[];
+  topics: string;
+  countries: string[];
+  countriesT: string[];
+  simpleAnswer: string;
+}
